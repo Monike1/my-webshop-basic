552cace57b3a3245ce8273a191a7e1a92c790703
Monika Woznowska <monika.woznowska@gmail.com>
2020-02-03 12:40:32 +0100

    adding a shop page, collection previw and collection item components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
-import HomePage from './pages/homepage/homepage.component';
-
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <Route exact path='/' component={HomePage} />
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
       </div>
     );
   }
